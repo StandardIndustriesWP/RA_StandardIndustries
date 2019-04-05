b7e696bfc76568e846fd008a662d20b47fa16fa5
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-05 15:06:07 -0400

    External link and intro

---
+++ modules/intro.php
@@ -0,0 +1,92 @@
+<?php
+
+use RA\Modules;
+
+$modules = Modules::singleton();
+
+$modules->add_module('Intro', 'intro', 'page_modules', [
+    array(
+        'key' => 'field_5ca78d2a79210',
+        'label' => 'Eyebrow',
+        'name' => 'eyebrow',
+        'type' => 'text',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+            'width' => '',
+            'class' => '',
+            'id' => '',
+        ),
+        'default_value' => '',
+        'placeholder' => '',
+        'prepend' => '',
+        'append' => '',
+        'maxlength' => '',
+    ),
+    array(
+        'key' => 'field_5ca78d5d79212',
+        'label' => 'Heading',
+        'name' => 'heading',
+        'type' => 'wysiwyg',
+        'instructions' => '',
+        'required' => 1,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+            'width' => '',
+            'class' => '',
+            'id' => '',
+        ),
+        'default_value' => '',
+        'tabs' => 'text',
+        'media_upload' => 1,
+        'toolbar' => 'full',
+        'delay' => 0,
+    ),
+    array(
+        'key' => 'field_5ca78d3379211',
+        'label' => 'Body',
+        'name' => 'body',
+        'type' => 'textarea',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+            'width' => '',
+            'class' => '',
+            'id' => '',
+        ),
+        'default_value' => '',
+        'placeholder' => '',
+        'maxlength' => '',
+        'rows' => '',
+        'new_lines' => 'wpautop',
+    ),
+    array(
+        'key' => 'field_5ca79e3ef1c79',
+        'label' => 'Type',
+        'name' => 'type',
+        'type' => 'select',
+        'instructions' => '',
+        'required' => 1,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+            'width' => '',
+            'class' => '',
+            'id' => '',
+        ),
+        'choices' => array(
+            'light' => 'Light',
+            'dark' => 'Dark',
+        ),
+        'default_value' => array(
+            0 => 'Light',
+        ),
+        'allow_null' => 0,
+        'multiple' => 0,
+        'ui' => 0,
+        'return_format' => 'value',
+        'ajax' => 0,
+        'placeholder' => '',
+    ),
+]);
