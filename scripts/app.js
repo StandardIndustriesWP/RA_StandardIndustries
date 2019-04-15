bb32c2ff5a2647adf7cdc68702d65579826dbdb4
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-15 14:59:14 -0400

    Team list info toggle and global button styles

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,9 @@ require('swiped-events');
 
 const Nav = require('./components/nav');
 const Spotlight = require('./modules/spotlight');
+const TeamList = require('./modules/team-list');
 
-const modules = [Nav, Spotlight];
+const modules = [Nav, Spotlight, TeamList];
 
 // Initialize all modules
 modules.forEach((module) => {
---
+++ scripts/modules/team-list.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * The element selector
+ * @type {string}
+ */
+const selector = '.team-list';
+
+class TeamList {
+  /**
+   * Component constructor, sets initial state
+   * @param el
+   */
+  constructor(el) {
+    this.el = el;
+    this.triggers = el.querySelectorAll('.team-list__learn-more');
+
+    // Bindings
+    this.toggleRegion = this.toggleRegion.bind(this);
+
+    // Listeners
+    this.triggers.forEach((trigger) => {
+      trigger.addEventListener('click', () => this.toggleRegion(trigger));
+    });
+  }
+
+  toggleRegion(trigger) {
+    const regionId = trigger.getAttribute('aria-controls');
+    const region = this.el.querySelector(`#${regionId}`);
+    const expanded = trigger.getAttribute('aria-expanded');
+
+    // Open this region
+    trigger.setAttribute(
+      'aria-expanded',
+      expanded === 'false' ? 'true' : 'false'
+    );
+    region.classList.toggle('team-list__body--expanded');
+    trigger.textContent = expanded === 'false' ? 'Show less' : 'Learn more';
+  }
+
+  /**
+   * Initializes all spotlights on a page
+   */
+  static init() {
+    const modules = document.querySelectorAll(selector);
+
+    modules.forEach((el) => {
+      new TeamList(el);
+    });
+  }
+}
+
+module.exports = TeamList;
