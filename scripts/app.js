52f8f30f7d3d12138908f137cc182e8242155230
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-22 12:32:31 -0400

    Working slideshow for press

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,11 @@ require('core-js/modules/es6.array.iterator');
 require('swiped-events');
 
 const Nav = require('./components/nav');
+const Press = require('./modules/press');
 const Spotlight = require('./modules/spotlight');
 const TeamList = require('./modules/team-list');
 
-const modules = [Nav, Spotlight, TeamList];
+const modules = [Nav, Press, Spotlight, TeamList];
 
 // Initialize all modules
 modules.forEach((module) => {
---
+++ scripts/modules/press.js
@@ -0,0 +1,105 @@
+'use strict';
+
+/**
+ * The element selector
+ * @type {string}
+ */
+const selector = '.press';
+
+class Press {
+  /**
+   * Component constructor, sets initial state
+   * @param el
+   */
+  constructor(el) {
+    // Elements
+    this.el = el;
+    this.items = el.querySelectorAll('.press__slide');
+    this.wrapper = el.querySelector('.press__slides');
+    this.navItems = el.querySelectorAll('.press__nav-item');
+
+    // State
+    this.slideCount = this.items.length;
+    this.activeSlide = 0;
+
+    // Bindings
+    this.prevSlide = this.prevSlide.bind(this);
+    this.nextSlide = this.nextSlide.bind(this);
+    this.setActiveSlide = this.setActiveSlide.bind(this);
+
+    // Listeners
+    this.wrapper.addEventListener('swiped-right', this.prevSlide);
+    this.wrapper.addEventListener('swiped-left', this.nextSlide);
+    this.navItems.forEach((item, i) => {
+      const button = item.querySelector('.press__nav-btn');
+      button.addEventListener('click', () => {
+        this.activeSlide = i;
+        this.setActiveSlide();
+      });
+    });
+
+    // Initialize
+    this.setActiveSlide();
+  }
+
+  /**
+   * Sets active slide to next slide
+   */
+  nextSlide() {
+    const nextSlide = this.activeSlide + 1;
+
+    this.activeSlide = nextSlide < this.slideCount ? nextSlide : 0;
+    this.setActiveSlide();
+  }
+
+  /**
+   * Sets active slide to previous slide
+   */
+  prevSlide() {
+    const prevSlide = this.activeSlide - 1;
+
+    this.activeSlide = prevSlide >= 0 ? prevSlide : this.slideCount - 1;
+    this.setActiveSlide();
+  }
+
+  /**
+   * Sets the active slide by transforming the wrapper
+   */
+  setActiveSlide() {
+    // Remove active class from all slides
+    this.items.forEach((item) => {
+      const link = item.querySelector('.press__link');
+
+      if (link) {
+        link.tabIndex = -1;
+      }
+      item.classList.remove('press__slide--active');
+    });
+
+    // Remove active class from all buttons
+    this.navItems.forEach((item) => {
+      item.classList.remove('press__nav-item--active');
+    });
+
+    // Add active class to activeSlide
+    const activeSlide = this.items[this.activeSlide];
+    const activeLink = activeSlide.querySelector('.press__link');
+
+    activeLink.tabIndex = 0;
+    this.items[this.activeSlide].classList.add('press__slide--active');
+    this.navItems[this.activeSlide].classList.add('press__nav-item--active');
+  }
+
+  /**
+   * Initializes all spotlights on a page
+   */
+  static init() {
+    const modules = document.querySelectorAll(selector);
+
+    modules.forEach((el) => {
+      new Press(el);
+    });
+  }
+}
+
+module.exports = Press;
