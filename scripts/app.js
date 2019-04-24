ada33012230a13ee64f005fd41a2443c02a521ee
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-24 16:02:48 -0400

    Basic search menu functionality

--- scripts/app.js
+++ scripts/app.js
@@ -7,10 +7,11 @@ require('swiped-events');
 const ContactForm = require('./modules/contact-form');
 const Nav = require('./components/nav');
 const Press = require('./modules/press');
+const Search = require('./components/search');
 const Spotlight = require('./modules/spotlight');
 const TeamList = require('./modules/team-list');
 
-const modules = [ContactForm, Nav, Press, Spotlight, TeamList];
+const modules = [ContactForm, Nav, Press, Search, Spotlight, TeamList];
 
 // Initialize all modules
 modules.forEach((module) => {
