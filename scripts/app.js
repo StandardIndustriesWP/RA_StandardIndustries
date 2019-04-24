6e86489db47a685b13de705dcfed725d2cf36269
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-24 12:34:55 -0400

    Contact form JS to allow for proper submit button styling/functionality

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,13 @@ require('core-js/modules/es6.promise');
 require('core-js/modules/es6.array.iterator');
 require('swiped-events');
 
+const ContactForm = require('./modules/contact-form');
 const Nav = require('./components/nav');
 const Press = require('./modules/press');
 const Spotlight = require('./modules/spotlight');
 const TeamList = require('./modules/team-list');
 
-const modules = [Nav, Press, Spotlight, TeamList];
+const modules = [ContactForm, Nav, Press, Spotlight, TeamList];
 
 // Initialize all modules
 modules.forEach((module) => {
