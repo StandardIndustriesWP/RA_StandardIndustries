9dda476a0e5f3a04521b5714651d8d92b175832c
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-25 12:03:40 -0400

    Search results page

--- search.php
+++ search.php
@@ -4,7 +4,9 @@ $templates = ['search.twig', 'archive.twig', 'index.twig'];
 
 $context = Timber::get_context();
 
-$context['title'] = 'Search results for ' . get_search_query();
+$search_query = $context['search_query'] = get_search_query();
+
+$context['result_count'] = count(Timber::get_posts("s={$search_query}&showposts=-1"));
 
 $context['posts'] = new Timber\PostQuery();
 
