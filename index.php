1b090600289e7edd58ce980dcb72a7b85390c782
Kevin Kneifel <kevin.kneifel@gmail.com>
2019-04-25 12:55:39 -0400

    Migrating tracking for this repo to the root

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-$context = Timber::get_context();
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-$context['posts'] = new Timber\PostQuery();
-
-$context['foo'] = 'bar';
-
-$templates = ['index.twig'];
-
-if (is_home()) {
-    array_unshift($templates, 'front-page.twig', 'home.twig');
-}
-
-Timber::render($templates, $context);
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
