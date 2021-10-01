07159b66b4ce8ebf6eef00934f7ea3289b637a89
Colleen Cleary <19698705+colleencleary@users.noreply.github.com>
2021-10-01 11:02:51 -0400

    add bg color selection to override default alternating backgrounds on team modules

--- wp-content/themes/standard-industries/modules/small-team-grid.php
+++ wp-content/themes/standard-industries/modules/small-team-grid.php
@@ -6,6 +6,34 @@ $modules = Modules::singleton();
 
 $modules->add_module('Small Team Grid', 'small-team-grid', 'page_modules', [
     array(
+        'key' => 'field_5cfa6c7464e18',
+        'label' => 'Section Background',
+        'name' => 'bg_color',
+        'type' => 'select',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+            'width' => '',
+            'class' => '',
+            'id' => '',
+        ),
+        'choices' => array(
+            'bg-default' => 'Default',
+            'bg-light' => 'Light',
+            'bg-dark' => 'Dark',
+        ),
+        'default_value' => array(
+            0 => 'Default',
+        ),
+        'allow_null' => 0,
+        'multiple' => 0,
+        'ui' => 0,
+        'return_format' => 'value',
+        'ajax' => 0,
+        'placeholder' => '',
+    ),
+    array(
         'key' => 'field_5cfa6c7464e16',
         'label' => 'Items',
         'name' => 'items',
